8ff3a33c4dce396a04744db5f66e9ffdc4f1e2ea
rakadin <hoangtranminhduc1005>
2023-12-17 21:50:17 +0700

    ticket_12: modify mushroom picker game

--- app/src/main/java/com/example/mathdices/part2/game_acitivity/Mushroom_Activity.java
+++ app/src/main/java/com/example/mathdices/part2/game_acitivity/Mushroom_Activity.java
@@ -7,12 +7,15 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.mathdices.R;
+import com.example.mathdices.RollDiceController;
 import com.example.mathdices.SoundControl;
 import com.example.mathdices.part2.Part2_Homepage_Activity;
 import com.example.mathdices.Gif_PopUp_Controller;
@@ -21,15 +24,15 @@ import com.example.mathdices.part2.winning_activity.Winning_activity_mushroom;
 import com.example.mathdices.utils.Utils;
 
 public class Mushroom_Activity extends AppCompatActivity {
-    ImageButton soundBut,homeBut,diceBut;
-    Gif_PopUp_Controller gif_popUp_controller = new Gif_PopUp_Controller();
-    SoundControl soundControl = new SoundControl();
-    Mushroom_Controller controller =  new Mushroom_Controller();
-    int diceNumFinal =0;
-    int count =0;
-    static final int dice_num[] = {6,12,34,35,50,67};// dice variable
-    Dialog dialog;
-    ImageButton move_0,move_1,move_2,move_3,move_4,move_5;
+    private ImageButton soundBut, homeBut, diceBut;
+    private Gif_PopUp_Controller gif_popUp_controller = new Gif_PopUp_Controller();
+    private SoundControl soundControl = new SoundControl();
+    private Mushroom_Controller controller = new Mushroom_Controller();
+    private int diceNumFinal = 0;
+    private int count = 0;
+    private Dialog dialog;
+    private ImageButton move_0, move_1, move_2, move_3, move_4, move_5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,32 +45,29 @@ public class Mushroom_Activity extends AppCompatActivity {
         diceBut = findViewById(R.id.dice);
 
         // set sound but func
-        soundControl.OnOffFun(Mushroom_Activity.this,soundBut);
+        soundControl.OnOffFun(Mushroom_Activity.this, soundBut);
         setHomeBut();
         rollDice();
         getmoveID();
         // set move_ids
-        ImageButton moves[] = {move_0,move_1,move_2,move_3,move_4,move_5};
+        ImageButton moves[] = {move_0, move_1, move_2, move_3, move_4, move_5};
 
 
-        for(int i=0;i<=5;i++)
-        {
-            int tem =i;
+        for (int i = 0; i <= 5; i++) {
+            int tem = i;
             moves[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    if(diceNumFinal == 0) // make sure that users have roll the dice first
+                    if (diceNumFinal == 0) // make sure that users have roll the dice first
                     {
-                        Toast.makeText(view.getContext(),"Bạn cần xúc xắc trước đã!",Toast.LENGTH_LONG).show();
-                    }
-                    else
-                    {
+                        Toast.makeText(view.getContext(), "Bạn cần xúc xắc trước đã!", Toast.LENGTH_LONG).show();
+                    } else {
                         soundControl.pianoSoundFunc(view.getContext());
                         gif_popUp_controller.show_mushroom_picker(dialog);
                         Utils.delay(40, () -> {
                             soundControl.fall.release();
                             dialog.dismiss();
-                            checkAns(tem,moves, view.getContext());
+                            checkAns(tem, moves, view.getContext());
                         });
                     }
 
@@ -75,11 +75,11 @@ public class Mushroom_Activity extends AppCompatActivity {
             });
         }
     }
-    void checkAns(int tem,ImageButton moves[],Context context)// checking ans
+
+    void checkAns(int tem, ImageButton moves[], Context context)// checking ans
     {
-        boolean check = controller.getAns(diceNumFinal,tem);
-        if(check == true)
-        {
+        boolean check = controller.getAns(diceNumFinal, tem);
+        if (check == true) {
             soundControl.correctSoundFun(Mushroom_Activity.this);
             gif_popUp_controller.show_mushroom_yeah(dialog);
             Utils.delay(40, () -> {
@@ -87,14 +87,11 @@ public class Mushroom_Activity extends AppCompatActivity {
                 moves[tem].setImageResource(xsign);// set xsign to mushroom
                 moves[tem].setClickable(false); // cant click after xsign
                 count++; // mushroom ++
-                if(count == 4)
-                {
-                    winning(context,dialog);
+                if (count == 4) {
+                    winning(context, dialog);
                 }
             });
-        }
-        else
-        {
+        } else {
             soundControl.wrongSoundFun(Mushroom_Activity.this);
             gif_popUp_controller.show_mushroom_sad(dialog);
             Utils.delay(50, () -> {
@@ -102,10 +99,10 @@ public class Mushroom_Activity extends AppCompatActivity {
             });
         }
     }
-    void winning(Context context,Dialog dialog)// check and open winning
+
+    void winning(Context context, Dialog dialog)// check and open winning
     {
-        if(count == 4)
-        {
+        if (count == 4) {
             soundControl.hooraySoundFun2(context);
             gif_popUp_controller.show_mushroom_yeah(dialog);
             Utils.delay(80, () -> {
@@ -117,9 +114,9 @@ public class Mushroom_Activity extends AppCompatActivity {
 
         }
     }
+
     // home button func
-    void setHomeBut()
-    {
+    void setHomeBut() {
         homeBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -129,25 +126,16 @@ public class Mushroom_Activity extends AppCompatActivity {
             }
         });
     }
+
     // roll the dicefunc
-    int rollDice()
-    {
+    int rollDice() {
         // dice button controll
         // roll the dice
         diceBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int images[] = {R.drawable.num_dice_6,R.drawable.num_dice_12,R.drawable.num_dice_34,R.drawable.num_dice_35,R.drawable.num_dice_50,R.drawable.num_dice_67};
-                int sec = 1;
-                for (int j = 0 ; j < 7;j++){
-                    Utils.delay(sec, () -> {
-                        soundControl.RollSoundFun(Mushroom_Activity.this);
-                        int tem = (int) (Math.random() * 6 + 1);
-                        diceNumFinal = dice_num[tem-1];
-                        diceBut.setImageResource(images[tem-1]);
-                    });
-                }
-
+                RollDiceController rollDiceController = new RollDiceController();
+                diceNumFinal = rollDiceController.rollTheNumDice(diceBut, view.getContext(), dialog);
             }
 
         });
@@ -156,8 +144,8 @@ public class Mushroom_Activity extends AppCompatActivity {
         return diceNumFinal;
 
     }
-    void getmoveID()
-    {
+
+    void getmoveID() {
         move_0 = findViewById(R.id.mushroom_1);
         move_1 = findViewById(R.id.mushroom_2);
         move_2 = findViewById(R.id.mushroom_3);
@@ -166,6 +154,7 @@ public class Mushroom_Activity extends AppCompatActivity {
         move_5 = findViewById(R.id.mushroom_6);
 
     }
+
     // if restart set activity again
     @Override
     protected void onRestart() {
@@ -173,12 +162,12 @@ public class Mushroom_Activity extends AppCompatActivity {
         soundBut.setImageResource(sound_on);
         soundControl.player.start();
     }
+
     // if destroy stop music
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(soundControl.player != null)
-        {
+        if (soundControl.player != null) {
             soundControl.player.stop();
         }
     }
